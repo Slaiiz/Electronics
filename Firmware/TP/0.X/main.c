d5144e2695f2b9e20773497357863a792cb24ca0
Labelec-vm <Labelec-vm>
2016-03-24 14:27:46 +0100

    firmware

---
+++ Firmware/TP/0.X/main.c
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.c
+ * Author: schiad
+ *
+ * Created on 23 mars 2016, 13:38
+ */
+
+#include <p32xxxx.h>
+
+int main(void)
+{
+    LATFbits.LATF1 = 1;
+    TRISFbits.TRISF1 = 0;
+    while(1)
+    {
+        LATFbits.LATF1 = 0;
+        LATFbits.LATF1 = 1;
+    }
+    return (0);
+}
+
---
+++ Firmware/TP/0.X/types.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   types.h
+ * Author: bocal
+ *
+ * Created on 23 mars 2016, 13:28
+ */
+
+#ifndef TYPES_H
+#define	TYPES_H
+
+#define u8  unsigned char
+#define u16 unsigned short
+#define u32 unsigned long
+#define s8  signed char
+#define s16 signed short
+#define s32 signed long
+#endif
