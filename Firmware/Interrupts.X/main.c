481abc02906dcb75fb6f25fff553aebf9fd50af4
Zaz <zaz@lol.com>
2016-03-25 17:37:13 +0100

    More documentation and work.

---
+++ Firmware/Interrupts.X/main.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.c
+ * Author: bocal
+ *
+ * Created on March 25, 2016, 5:11 PM
+ */
+
+#include <xc.h>
+#include "types.h"
+
+int main(void)
+{
+    
+    return (0);
+}
---
+++ Firmware/Interrupts.X/types.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   types.h
+ * Author: bocal
+ *
+ * Created on March 25, 2016, 5:12 PM
+ */
+
+#ifndef TYPES_H
+#define	TYPES_H
+
+typedef unsigned char   u8;
+typedef unsigned short  u16;
+typedef unsigned long   u32;
+
+typedef signed char     s8;
+typedef signed short    s16;
+typedef signed long     s32;
+
+#endif	/* TYPES_H */
+
